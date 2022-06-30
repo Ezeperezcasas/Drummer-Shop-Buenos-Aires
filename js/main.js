dc50f674eb202b5253b3b2ed53fff5a20a86a4cc
Ezeperezcasas <ezeperezcasas@gmail.com>
2022-06-30 09:56:23 -0300

    Primera subida Java Script

---
+++ js/main.js
@@ -0,0 +1,20 @@
+ let numeros = [10,20,30,40,50,60,70,80,90];
+
+for (let i = 0; i < numeros.length;i++) {
+    console.log(numeros[i]);
+    
+} 
+
+
+
+let contador = 0;
+
+while(contador < 10){
+    console.log(contador);
+    contador ++;
+
+}
+
+
+
+/* En el primer ejemplo aplico FOR y en el segundo ejemplo aplico WHILE */
